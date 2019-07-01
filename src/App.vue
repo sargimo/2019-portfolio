a37f0346917899ded28f464954c7d4c2dc66369a
Geoffrey philip james Sargison <932400987@dov2-sc-20.local>
2019-07-01 16:10:40 +1200

    base vue skeleton

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>-->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  body {
+    position: absolute;
+    top: 0;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    background: url("./assets/mt-cook.jpg") center / cover;
+    overflow-y: hidden;
+    #app {
+      font-family: "Avenir", Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      width: 100%;
+      margin: 0;
+      padding: 0;
+    }
+  }
+}
+
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div class="menu">
+      <i class="fas fa-ellipsis-h"></i>
+      <div class="columns">
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+      </div>
+    </div>
+    <div class="menu">
+      <i class="fas fa-ellipsis-h"></i>
+      <div class="columns">
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+      </div>
+    </div>
+    <div class="menu">
+      <i class="fas fa-ellipsis-h"></i>
+      <div class="columns">
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+        <div class="col">content</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu"
+};
+</script>
+
+<style lang="scss">
+.container {
+  width: 70%;
+  margin: 5% auto;
+//   background: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  .menu {
+    width: 40px;
+    height: 40px;
+    background: rgba(249, 249, 249, 0.8);
+    // margin: 5%;
+    transition: all 0.4s ease;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .columns {
+      display: none;
+    }
+    &:hover {
+      cursor: pointer;
+      width: 80vw;
+      height: 70vh;
+      // margin: 20px;
+      transition: all 0.4s ease-in-out;
+      .fas {
+        display: none;
+      }
+      .columns {
+        height: 100%;
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0;
+        flex-wrap: wrap;
+        .col {
+          padding: 0;
+          width: 30%;
+          background: rgba(0, 0, 0, 0.6);
+          height: 100px;
+        }
+      }
+    }
+  }
+}
+</style>
